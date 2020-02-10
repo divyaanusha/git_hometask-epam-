b0df9f6406e8700ac518c0ae1a29823884ad23af
divya anusha <anuchandrupatla13@gmail.com>
2020-02-10 07:20:25 +0530

    first commit

---
+++ src/main/java/git_intro/hometask_git/App.java
@@ -0,0 +1,13 @@
+package git_intro.hometask_git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
